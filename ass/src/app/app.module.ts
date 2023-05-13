27fa4f8f514cface91f03fdad988bc9fa7b8914e
dinhqtai <dinhquoctai2003@gmail.com>
2023-05-13 19:47:20 +0700

    main

--- ass/src/app/app.module.ts
+++ ass/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -21,7 +21,8 @@ import { HeaderAdminComponent } from './layout/header-admin/header-admin.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
