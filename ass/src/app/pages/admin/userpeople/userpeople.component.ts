151d3322fe60745123a94ee029d01bf2bdd215e9
HoangThuNgoc <110533204+HoangThuNgoc@users.noreply.github.com>
2023-05-21 21:12:04 +0700

    delete

--- ass/src/app/pages/admin/userpeople/userpeople.component.ts
+++ ass/src/app/pages/admin/userpeople/userpeople.component.ts
@@ -11,4 +11,11 @@ getUser: any[]=[];
 ngOnInit(): void {
   axios.get("http://localhost:8088/api/user").then((data)=>{this.getUser =data.data})
 }
+UserDelete(id: string): void {
+  const isConfirm = confirm("ban co chắc xóa sản phẩm này không");
+  if(isConfirm) {
+    this.getUser = this.getUser.filter(user =>  user._id != id);
+    axios.delete(`http://localhost:8088/api/user/`+ id).then(() => alert("Bạn xóa thành công"));
+  }
+}
 }
