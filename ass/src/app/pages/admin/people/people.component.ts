d381522f149f3a0a1f2ce785397ff65535facab6
Nguyễn Ngọc Hoàng <hoang122003k@gmail.com>
2023-05-23 02:22:28 +0700

    main

--- ass/src/app/pages/admin/people/people.component.ts
+++ ass/src/app/pages/admin/people/people.component.ts
@@ -7,9 +7,15 @@ import axios from 'axios';
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
-  getUser: any[]=[];
-  ngOnInit(): void {
-    axios.get("http://localhost:8088/api/user").then((data)=>{this.getUser =data.data})
+getUser: any[]=[];
+ngOnInit(): void {
+  axios.get("http://localhost:8088/api/user").then((data)=>{this.getUser =data.data})
+}
+UserDelete(id: string): void {
+  const isConfirm = confirm("ban co chắc xóa sản phẩm này không");
+  if(isConfirm) {
+    this.getUser = this.getUser.filter(user =>  user._id != id);
+    axios.delete(`http://localhost:8088/api/user/`+ id).then(() => alert("Bạn xóa thành công"));
   }
 }
-
+}
