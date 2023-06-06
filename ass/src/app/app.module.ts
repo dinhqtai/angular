a3a3495d4e39b23fdb52b3b3e6ca20dd35998cd6
HoangThuNgoc <110533204+HoangThuNgoc@users.noreply.github.com>
2023-06-06 23:40:42 +0700

    phantrangg

--- ass/src/app/app.module.ts
+++ ass/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +47,8 @@ import { ProductsComponent } from './pages/user/products/products.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
